7ee669c66419785c98339b1d6473b122adcdffb2
Ceyda97 <97360357+Ceyda97@users.noreply.github.com>
2022-04-14 18:17:16 +0100

    creating new repository from github account

--- src/Practice1.java
+++ src/Practice1.java
@@ -6,4 +6,12 @@ public class Practice1 {
 
     //step 3
 
+    /*
+    how to create a new repository from your github account?
+    from your github account, click + button from right top side and click new repository
+    enter your repository name, choose public or private. go all the way down and click create repository
+
+     */
+
+
 }
